6bbbfa7ea91fdba3e0b0a07c30709c802e521950
Eljas Veijalainen <114503308+EljasV@users.noreply.github.com>
2023-08-03 18:08:08 +0300

    Exercise 12

--- rate-repository-app/src/utils/apolloClient.js
+++ rate-repository-app/src/utils/apolloClient.js
@@ -1,8 +1,8 @@
-import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import {ApolloClient, InMemoryCache, createHttpLink} from '@apollo/client';
+import Constants from "expo-constants";
 
 const httpLink = createHttpLink({
-    // Replace the IP address part with your own IP address!
-    uri: 'http://192.168.11.3:4000/graphql',
+    uri: Constants.expoConfig.extra.apollo_uri,
 });
 
 const createApolloClient = () => {
